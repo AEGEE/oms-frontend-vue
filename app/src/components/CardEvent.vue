c7b01bc498914b59c157a5c4019d4171b4720bce
i3anaan <i3anaan@gmail.com>
2018-05-01 14:11:33 +0200

    add: first card!

---
+++ app/src/components/CardEvent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='card'>
+        <div class="title">{{event.title}}</div>
+        <div class="description">{{event.description}}</div>
+        <img v-bind:src="event.imageurl"></img>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    props: ['event'],
+});
+</script>
+
+<style>
+.title {
+    font-size: 20px;
+}
+.description {
+    font-size: 15px;
+}
+
+</style>
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import HelloComponent from "./components/Hello.vue";
 import HelloDecoratorComponent from "./components/HelloDecorator.vue";
+import CardEvent from "./components/CardEvent.vue"
 
 let v = new Vue({
     el: "#app",
@@ -11,11 +12,20 @@ let v = new Vue({
         <hello-component :name="name" :initialEnthusiasm="5" />
         <h1>Hello Decorator Component</h1>
         <hello-decorator-component :name="name" :initialEnthusiasm="5" />
+        <card-event :event="event"/>
         </div>
     `,
-    data: { name: "World" },
+    data: {
+      name: "World",
+      event: {
+        title: 'Hackathon',
+        description: 'Building OMS from Krakow',
+        imageurl: 'https://sekurak.pl/wp-content/uploads/2016/10/shp_krak-e1476525429173-600x450.jpg',
+      },
+    },
     components: {
         HelloComponent,
-        HelloDecoratorComponent
+        HelloDecoratorComponent,
+        CardEvent,
     }
 });
